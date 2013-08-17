d5e57c594715d869053abd93801e14c5cc42dd0d
raynoe <raynoe_lxd@hotmail.com>
2013-08-17 19:59:23 +0700

    add user

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+
+	has_many :user
+
+	validates :user_id, :price, presence: true
+	validates :title, length: {minimum: 10 }, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130817125158) do
+ActiveRecord::Schema.define(version: 20130817125320) do
 
   create_table "products", force: true do |t|
     t.string   "code"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20130817125158) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
